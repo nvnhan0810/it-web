e3394abdf5a9c50201d4f709ed760ea1d3a8a131
nguyenvannhan <nguyenvannhan0810@gmail.com>
2025-01-21 21:37:40 +0700

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,16 @@ set -e
 
 fnm use
 
+npm ci
+npm run build
+
 npm i -g pm2
 
-PM2_EXIST=$(if pm2 list 2>/dev/null | grep -q nvnahn0810-it-web; then echo "Yes"; else echo "No"; fi)
+PM2_EXIST=$(if pm2 list 2>/dev/null | grep -q nvnhan0810-it-web; then echo "Yes"; else echo "No"; fi)
 
 if [ $PM2_EXIST = Yes ]; then
-	pm2 stop nvnahn0810-it-web
-	pm2 delete nvnahn0810-it-web
+	pm2 stop nvnhan0810-it-web
+	pm2 delete nvnhan0810-it-web
 fi
 
-pm2 --name nvnahn0810-it-web start npm -- start -- -p 3002
+pm2 --name nvnhan0810-it-web start npm -- start -- -p 3002
